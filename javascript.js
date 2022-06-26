17cae83914edd7f36ff91dd423810dee01dd3a92
Cesar <cesr.sntn@gmail.com>
2022-06-26 12:45:57 -0400

    finish code and project runs as intended

--- javascript.js
+++ javascript.js
@@ -1,37 +1,97 @@
 
+
+
+
 const crock = "rock";
 const cpaper = "paper";
 const cscissors = "scissors";
-
-let random = Math.floor(Math.random()*12)+1;
-let compChoice;
 
 const urock = "rock";
 const upaper = "paper";
 const uscissors = "scissors";
 
 
+let play = confirm("Do you want to play Rock, Paper, Scissors?");
 
-if (random <=4){
-    compChoice = crock;
-}else if (random > 4 && random <= 8){
-    compChoice = cpaper
+if (play == true){
+    RPS();
 }else{
-    compChoice = cscissors;
+    alert("Have a great day!");
 }
 
-let user = prompt("Rock, Paper, or Scissors?");
-user = user.toLowerCase();
 
-if(user == "rock"){
- user = urock;
-}else if(user == "paper"){
- user = upaper
-}else if (user == "scissors"){
- user = uscissors;
+
+function RPS(){
+
+    let compScore = 0;
+    let userScore = 0
+ 
+for (let i = 1; i <=5; i++){
+    
+    let user = prompt("Rock, Paper, or Scissors?");
+    user = user.toLowerCase();
+
+    let compChoice;
+    let random = Math.floor(Math.random()*12)+1;
+    if (random <=4){
+        compChoice = crock;
+    }else if (random > 4 && random <= 8){
+        compChoice = cpaper
+    }else{
+        compChoice = cscissors;
+    }
+    
+    
+    if(user == "rock"){
+     user = urock;
+    }else if(user == "paper"){
+     user = upaper
+    }else if (user == "scissors"){
+     user = uscissors; 
+    }
+
+    if(user == urock && compChoice == cpaper){
+        alert("Computer won this round!");
+        compScore++;
+
+    }else if (user == urock && compChoice == cscissors){
+        alert("You won this round!");
+        userScore++;
+    }else if(user == upaper && compChoice == crock){
+        alert("You won this round!");
+        userScore++;
+    }else if(user == upaper && compChoice == cscissors){
+        alert("Computer won this round!");
+        compScore++;
+    }else if(user == uscissors && compChoice == crock){
+        alert("Computer won this round!");
+        compScore++;
+    }else if(user == uscissors && compChoice == cpaper){
+        alert("You won this round!");
+        userScore++;
+    }else{
+        alert("This round was a tie.");
+    }
+
+    alert("Games won: \n You: " + userScore + "\n Computer: "+compScore)
+    console.log("computer: "+compChoice);
+    console.log("User: "+user);
+}
+if(userScore > compScore){
+    alert("You won the game!")
+}else if (userScore < compScore){
+    alert("Sorry, you lost the game.")
+}else{
+    alert("The game is a tie!");
 }
 
-console.log(user);
-console.log(compChoice);
+let playAgain = confirm("Do you want to play again?");
 
+if (playAgain == true){
+    RPS();
+}else{
+    alert("Thank you for playing :)");
+    
+}
 
+}
