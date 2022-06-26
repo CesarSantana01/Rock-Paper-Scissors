2d12cca2f084129f9922a686bba249eb8eb4f0f5
Cesar <cesr.sntn@gmail.com>
2022-06-25 21:54:45 -0400

    Create user variable that stores user input.
    User variable is meant to hold rock paper or scissors and automatically convert the variable to lower case. This is to then compare it to compChoice and decide who is the winner between the two.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,12 @@ const cscissors = "scissors";
 let random = Math.floor(Math.random()*12)+1;
 let compChoice;
 
+const urock = "rock";
+const upaper = "paper";
+const uscissors = "scissors";
+
+
+
 if (random <=4){
     compChoice = crock;
 }else if (random > 4 && random <= 8){
@@ -13,5 +19,19 @@ if (random <=4){
 }else{
     compChoice = cscissors;
 }
+
+let user = prompt("Rock, Paper, or Scissors?");
+user = user.toLowerCase();
+
+if(user == "rock"){
+ user = urock;
+}else if(user == "paper"){
+ user = upaper
+}else if (user == "scissors"){
+ user = uscissors;
+}
+
+console.log(user);
+console.log(compChoice);
 
 
